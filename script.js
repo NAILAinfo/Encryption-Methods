4f32c0e4e811e20f5096c3e9b6a5ddf98d2f73b0
NAILAinfo <nailadahmani.info@gmail.com>
2025-03-09 11:03:01 +0100

    definir OTP et modification css

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-function estMinuscule(c) {
+export function estMinuscule(c) {
     return c >= 'a' && c <= 'z';
 }
-function valeurNum(c){
+export function valeurNum(c){
     return c.charCodeAt(0) - 65;
 }
-function valeurAlpha(c){
+export function valeurAlpha(c){
     return String.fromCharCode(c + 65);
+}
+export function vignere(msg, cle) {
+    let result = "";
+    msg = msg.toUpperCase().replace(/[^A-Z]/g, ""); 
+    cle = cle.toUpperCase().replace(/[^A-Z]/g, ""); 
+
+    if (cle.length === 0) {
+        return "Erreur : la clé doit contenir au moins une lettre.";
+    }
+
+    for (let i = 0, j = 0; i < msg.length; i++) {
+        let n = valeurNum(msg[i]);
+        let k = valeurNum(cle[j % cle.length]);
+        let newCharCode = (n + k) % 26;
+        result += valeurAlpha(newCharCode);
+        j++; 
+    }
+    return result;
 }

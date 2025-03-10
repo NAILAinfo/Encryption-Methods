536ec874e42afb95733810ac4e9fd921fd19b874
NAILAinfo <mhammed.dahmani66@gmail.com>
2025-03-10 12:32:52 +0100

    ajouter les dechiffrement et le calcule d'indice de coincidence

--- script.js
+++ script.js
@@ -7,6 +7,26 @@ export function valeurNum(c){
 export function valeurAlpha(c){
     return String.fromCharCode(c + 65);
 }
+
+
+export function cesare(msg, decalage) {
+    let result = "";
+    msg = msg.toUpperCase();
+
+    for (let i = 0; i < msg.length; i++) {
+        let char = msg[i];
+        if (char >= 'A' && char <= 'Z') {
+            let codeChiffre = (valeurNum(char) + decalage) % 26;
+            if (codeChiffre < 0) {
+                codeChiffre += 26; 
+            }
+            result += valeurAlpha(codeChiffre);
+        } else {
+            result += char;
+        }
+    }
+    return result;
+}
 export function vignere(msg, cle) {
     let result = "";
     msg = msg.toUpperCase().replace(/[^A-Z]/g, ""); 
@@ -20,6 +40,24 @@ export function vignere(msg, cle) {
         let n = valeurNum(msg[i]);
         let k = valeurNum(cle[j % cle.length]);
         let newCharCode = (n + k) % 26;
+        result += valeurAlpha(newCharCode);
+        j++; 
+    }
+    return result;
+}
+export function vignereD(msg, cle) {
+    let result = "";
+    msg = msg.toUpperCase().replace(/[^A-Z]/g, ""); 
+    cle = cle.toUpperCase().replace(/[^A-Z]/g, ""); 
+
+    if (cle.length === 0) {
+        return "Erreur : la clé doit contenir au moins une lettre.";
+    }
+
+    for (let i = 0, j = 0; i < msg.length; i++) {
+        let n = valeurNum(msg[i]);
+        let k = valeurNum(cle[j % cle.length]);
+        let newCharCode = (n - k+ 26) % 26;
         result += valeurAlpha(newCharCode);
         j++; 
     }
